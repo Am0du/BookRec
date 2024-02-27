71b65a03582d2314535fa363c3b921e7f98232a4
Amodu <amodudavid777@gmail.com>
2024-02-27 13:45:51 +0100

    change environment variable

--- models/database.py
+++ models/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.exc import NoResultFound, IntegrityError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
-engine = create_engine(os.environ.get('sqlite'))
+engine = create_engine(os.environ.get('postgres'))
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
