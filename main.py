69555c0ebe9a4bf7ed6d6f9577749bfcc9558184
Amodu <amodudavid777@gmail.com>
2024-02-27 21:58:10 +0100

    added comment

--- main.py
+++ main.py
@@ -34,13 +34,23 @@ def token_required(func):
 def header_detail():
     token = request.headers.get('Authorization')
     token_data = jwt.decode(token, app.secret_key, algorithms=['HS256'])
-    # author = find_author(token_data.get('email'))
+
     return token_data.get('email')
 
-
+#ROUTES
 @app.route('/api/register', methods=['POST'])
 def register():
-    """ Register Author"""
+    """
+    Register Author
+
+    This function registers a new author by extracting data from a JSON request.
+
+    Parameters:
+        None (uses data from the request)
+
+    Returns:
+        A JSON response indicating the success or failure of the registration process.
+    """
     data = request.get_json()
     name = data.get('name')
     email = data.get('email')
@@ -55,15 +65,25 @@ def register():
             return jsonify(isSuccessful='true', redirectUri=url_for('login'), info='User created'), 201
         else:
             return jsonify(isSuccessful='false', info='User already exist', status_code=409), 409
-
-
     else:
         return jsonify(isSuccessful='false', info='User data not complete'), 400
 
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    """ Login Author """
+    """
+        Login Author
+
+        This function handles the login process for an author by extracting email and password
+        from the JSON data obtained from the request.
+
+        Parameters:
+            None (uses data from the request)
+
+        Returns:
+            A JSON response indicating the success or failure of the login process.
+        """
+
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
@@ -86,7 +106,18 @@ def login():
 @app.route('/api/add_book', methods=['POST'])
 @token_required
 def add_book():
-    """Add book to an Author"""
+    """
+    Add book to an Author
+
+    This function allows an author to add a new book by extracting necessary data from the request JSON.
+
+    Parameters:
+        None (uses data from the request and header)
+
+    Returns:
+        A JSON response indicating the success or failure of the book addition process.
+    """
+
     email = header_detail()
     author = find_author(email)
 
@@ -118,7 +149,18 @@ def add_book():
 @app.route('/api/edit_book', methods=['PUT'])
 @token_required
 def edit_author_book():
-    """ Author edits his books """
+    """
+        Author edits his books
+
+        This function allows an author to edit their books by providing new information.
+
+        Parameters:
+            None (uses data from the request and header)
+
+        Returns:
+            A JSON response indicating the success or failure of the book editing process.
+    """
+
     data = request.get_json()
     title = data.get('title')
     new_title = data.get('new_title')
@@ -157,8 +199,6 @@ def edit_author_book():
 
                 return jsonify(result), 200
 
-            # return jsonify(isSuccessful='false', info='Book does not exist or unAuthorized', status_code=404), 404
-
     if find_book_title(title):
         return jsonify(isSuccessful='false', info=f'Not authorized to delete {title}', status_code=401), 401
     else:
@@ -173,7 +213,18 @@ def edit_author_book():
 @app.route('/api/edit_author', methods=['PUT'])
 @token_required
 def edit_detail():
-    """ Author edits his details """
+    """
+        Author edits his details
+
+        This function allows an author to edit their details such as name and about section.
+
+        Parameters:
+            None (uses data from the request and header)
+
+        Returns:
+            A JSON response indicating the success or failure of the author details editing process.
+        """
+
     author_email = header_detail()
     data = request.get_json()
     name = data.get('name')
@@ -207,7 +258,18 @@ def edit_detail():
 @app.route('/api/delete_book', methods=['DELETE'])
 @token_required
 def delete():
-    """ Author deletes his books """
+    """
+       Author deletes his books
+
+       This function allows an author to delete their books by providing the title of the book.
+
+       Parameters:
+           None (uses data from the request and header)
+
+       Returns:
+           A JSON response indicating the success or failure of the book deletion process.
+    """
+
     author_email = header_detail()
     data = request.get_json()
     title = data.get('title')
@@ -227,8 +289,18 @@ def delete():
 
 
 @app.route('/api/genre/<genre>', methods=['GET'])
-def genre(genre):
-    """" Returns books by with requested genre"""
+def find_genre(genre):
+    """
+    Returns books with requested genre
+
+    This function retrieves books with the specified genre.
+
+    Parameters:
+        genre (str): The genre of the books to retrieve
+
+    Returns:
+        A JSON response containing books with the requested genre, or a message indicating the genre was not found.
+    """
     book_list = all_books()
     genre_list = []
     for book in book_list:
@@ -248,7 +320,18 @@ def genre(genre):
 
 @app.route('/api/book', methods=['GET'])
 def book():
-    """ Returns books with the same title"""
+    """
+       Returns books with the same title
+
+       This function retrieves details of a book with the specified title.
+
+       Parameters:
+           None (uses data from the request)
+
+       Returns:
+           A JSON response containing details of the book with the requested title, or a message indicating the book was not found.
+       """
+
     data = request.get_json()
     title = data.get('title')
     result = find_book_title(title)
@@ -270,7 +353,17 @@ def book():
 
 @app.route('/api/books', methods=['GET'])
 def books():
-    """ Returns all books"""
+    """
+        Returns all books
+
+        This function retrieves details of all available books.
+
+        Parameters:
+            None
+
+        Returns:
+            A JSON response containing details of all available books.
+    """
     data = all_books()
     data_dict = {}
     for i in data:
